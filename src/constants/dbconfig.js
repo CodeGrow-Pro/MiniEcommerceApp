ddf658be371f16eb04730eceeb9e3d168d54e01e
VishalDevelooperengineer <vk4993102@gmail.com>
2022-11-20 21:57:35 +0530

    old version

--- src/constants/dbconfig.js
+++ src/constants/dbconfig.js
@@ -1,21 +1,21 @@
-// module.exports = {
-//     mysql:{
-//         prod:{
-//             host:"localhost",
-//             user:'root',
-//             password:'vishal',
-//             database:'miniecommerceapp'
-//         }
-//     }
-// }
-
 module.exports = {
     mysql:{
         prod:{
-            host:"sql112.epizy.com",
-            user:'epiz_32926867',
-            password:'Az4s0gQ6f4xNy',
-            database:'epiz_32926867_miniecommerceapp'
+            host:"localhost",
+            user:'root',
+            password:'vishal',
+            database:'miniecommerceapp'
         }
     }
 }
+
+// module.exports = {
+//     mysql:{
+//         prod:{
+//             host:"sql112.epizy.com",
+//             user:'epiz_32926867',
+//             password:'Az4s0gQ6f4xNy',
+//             database:'epiz_32926867_miniecommerceapp'
+//         }
+//     }
+// }
